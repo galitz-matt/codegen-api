eb5a2f00c4540aa5fcd84374c3ab2dde5366d997
galitz-matt <galitz.matthew@gmail.com>
2026-02-10 21:51:29 -0500

    feat: param ir constructor

---
+++ src/dsl/param.ts
@@ -0,0 +1,36 @@
+import { Param } from "../ir/params";
+
+export function param(
+    name: string,
+    type: string
+): Param {
+    return {
+        kind: "std",
+        name,
+        type
+    };
+}
+
+export function defaultParam(
+    name: string,
+    type: string,
+    def: string,
+): Param {
+    return {
+        kind: "default",
+        name,
+        type,
+        default: def
+    };
+}
+
+export function optionalParam(
+    name: string,
+    type: string,
+): Param {
+    return {
+        kind: "optional",
+        name,
+        type
+    }
+}
