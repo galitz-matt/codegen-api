025575722103e4f3b70c586a502211a570e6b865
galitz-matt <galitz.matthew@gmail.com>
2026-02-10 23:23:03 -0500

    refactor: auto-resolve close

--- src/dsl/block.ts
+++ src/dsl/block.ts
@@ -3,12 +3,11 @@ import { Doc } from "../ir/doc";
 export function block(
     open: string,
     body: Doc[],
-    close: string,
 ): Doc {
     return {
         kind: "block",
         open,
         body,
-        close
+        close: "}"
     }
 }
