2dac25b789f3ee026e65d34051a98c8b90a957c4
galitz-matt <galitz.matthew@gmail.com>
2026-02-12 00:01:36 -0500

    feat: accept rest params, use new branch types

--- src/dsl/cond.ts
+++ src/dsl/cond.ts
@@ -1,37 +1,30 @@
-import { Branch, DefaultBranch } from "../ir/branch";
+import { Branch, DefaultBranch, WhenBranch } from "../ir/branch";
 import { Doc } from "../ir/doc";
 import { block } from "./block";
 import { seq } from "./seq";
 
 export function cond(
-    branches: Branch[],
-    otherwise?: DefaultBranch
+    ...branches: Branch[]
 ): Doc {
-    const [first, ...rest] = branches;
+    const whens: WhenBranch[] = [];
+    let def: DefaultBranch | undefined;
 
-    if (otherwise) {
-        return seq(
-            block(`if (${first.discriminator}) {`,
-                first.body
-            ),
-            ...rest.map(b => 
-                block(`else if (${b.discriminator}) {`,
-                    b.body
-                )
-            ),
-            block(`else {`, 
-                otherwise.body
-            )
-        );
+    for (const b of branches) {
+        if (b.kind === "when") whens.push(b);
+        else if (b.kind === "default") def = b;
+        else throw new Error(`Incompatible branch type: ${b.kind}`);
     }
+
+    const [first, ...rest] = whens;
+    if (!first) {
+        throw new Error("cond() requires at least one 'when' branch");
+    }
+
     return seq(
-        block(`if (${first.discriminator}) {`,
-            first.body
-        ),
+        block(`if (${first.condition}) {`, ...first.body),
         ...rest.map(b => 
-            block(`else if (${b.discriminator}) {`,
-                b.body
-            )
-        )
+            block(`else if (${b.condition}) {`, ...b.body)
+        ),
+        ...(def ? [block("else {", ...def.body)] : [])
     );
 }
