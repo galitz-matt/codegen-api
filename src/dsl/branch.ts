8df359d585177e81e10d8b7d3d3c8dbe9216f49e
galitz-matt <galitz.matthew@gmail.com>
2026-02-12 00:01:00 -0500

    feat: use new branch types, rest params

--- src/dsl/branch.ts
+++ src/dsl/branch.ts
@@ -1,22 +1,25 @@
-import { Branch, DefaultBranch } from "../ir/branch"
+import { Branch } from "../ir/branch"
 import { Doc } from "../ir/doc"
 
-export function when(condition: string, body: Doc[]): Branch {
+export function when(condition: string, ...body: Doc[]): Branch {
     return {
-        discriminator: condition,
+        kind: "when",
+        condition,
         body
     }
 }
 
-export function caseOf(value: string, body: Doc[]): Branch {
+export function caseOf(value: string, ...body: Doc[]): Branch {
     return {
-        discriminator: value,
+        kind: "case",
+        value,
         body
     }
 }
 
-export function otherwise(body: Doc[]): DefaultBranch {
+export function otherwise(...body: Doc[]): Branch {
     return {
+        kind: "default",
         body
     }
 }
