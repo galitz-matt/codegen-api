43874716dbf3bb3f6ecf12a7773acdf83a7310ab
galitz-matt <galitz.matthew@gmail.com>
2026-02-12 00:00:27 -0500

    refactor: define discriminated union of branches, extract specific types

--- src/ir/branch.ts
+++ src/ir/branch.ts
@@ -1,10 +1,21 @@
 import { Doc } from "./doc"
 
-export type Branch = {
-    discriminator: string;
+export type WhenBranch = Extract<Branch, { kind: "when" }>
+export type CaseBranch = Extract<Branch, { kind: "case" }>
+export type DefaultBranch = Extract<Branch, { kind: "default" }>
+
+export type Branch = 
+| {
+    kind: "when";
+    condition: string;
     body: Doc[];
 }
-
-export type DefaultBranch = {
+| {
+    kind: "case";
+    value: string;
+    body: Doc[];
+}
+| {
+    kind: "default";
     body: Doc[];
 }
