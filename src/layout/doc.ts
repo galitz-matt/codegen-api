d2df5d8120240a3407d3b087b345c8b00c92caed
galitz-matt <galitz.matthew@gmail.com>
2026-02-13 16:45:33 -0500

    feat: extraact Doc subtypes

--- src/layout/doc.ts
+++ src/layout/doc.ts
@@ -1,3 +1,7 @@
+export type Line = Extract<Doc, { kind: "line" }>
+export type Block = Extract<Doc, { kind: "block" }>
+export type Seq = Extract<Doc, {kind: "block" }>
+
 export type Doc =
     | { kind: "line", text: string }
     | { kind: "block", open: string, body: Doc[], close: string }
