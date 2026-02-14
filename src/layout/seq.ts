cda29336b1a8cb2f12051b0c71be99c3129cec78
galitz-matt <galitz.matthew@gmail.com>
2026-02-14 12:38:12 -0500

    feat: constrain seq arguments to Line or Block

--- src/layout/seq.ts
+++ src/layout/seq.ts
@@ -1,7 +1,8 @@
-import { Doc } from "./doc";
+import { render } from "../render";
+import { Block, Doc, Line } from "./doc";
 
 export function seq(
-    ...docs: Doc[]
+    ...docs: (Line | Block)[]
 ): Doc {
     return {
         kind: "seq",
