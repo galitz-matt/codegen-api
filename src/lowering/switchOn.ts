75c11f2b661ca3a6e75e93b3ad7d061ad9ae4705
galitz-matt <galitz.matthew@gmail.com>
2026-02-21 15:46:58 -0500

    feat: implement switchOn function

--- src/lowering/switchOn.ts
+++ src/lowering/switchOn.ts
@@ -1,9 +1,21 @@
-import { Branch } from "../fragments/types";
+import { caseOf, otherwise } from "../fragments/factories";
+import { Branch, CaseBranch, DefaultBranch } from "../fragments/types";
+import { block } from "../layout/factories";
 import { Node } from "../layout/ir";
 
 export function switchOn(
     expr: string,
-    ...branches: Branch[]
+    ...branches: [...CaseBranch[], DefaultBranch] | CaseBranch[] | []
 ): Node {
-
+    return block(expr,
+        ...branches.map(b => {
+            if (b.kind === "case") {
+                return block(`case: ${b.value}`, 
+                    ...b.body);
+            } else {
+                return block(`default:`, 
+                    ...b.body);
+            }
+        })
+    );
 }
