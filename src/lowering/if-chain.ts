8ff2c55b52fd872d2ba15e49711505a2b963f6d9
galitz-matt <galitz.matthew@gmail.com>
2026-02-21 15:27:06 -0500

    feat: implement ifChain

--- src/lowering/if-chain.ts
+++ src/lowering/if-chain.ts
@@ -1,9 +1,27 @@
 import { WhenBranch, DefaultBranch } from "../fragments/types";
+import { block, seq } from "../layout/factories";
 import { Node } from "../layout/ir";
+import { render } from "../render";
 
 export function ifChain(
     first: WhenBranch,
     ...rest: [...WhenBranch[], DefaultBranch]
 ): Node {
-
+    return seq(
+        block(`if (${first.condition})`, 
+            ...first.body
+        ),
+        ...rest.map(b => {
+            switch (b.kind) {
+                case "when":
+                    return block(`else if (${b.condition})`,
+                        ...b.body
+                    );
+                case "default":
+                    return block(`else`,
+                        ...b.body
+                    );
+            }
+        })
+    );   
 }
