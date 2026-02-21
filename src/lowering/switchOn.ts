41a2c7e6bca70fe35cf4687da07238b6ab4a9535
galitz-matt <galitz.matthew@gmail.com>
2026-02-21 15:01:32 -0500

    refactor: return node

--- src/lowering/switchOn.ts
+++ src/lowering/switchOn.ts
@@ -1,8 +1,9 @@
 import { Branch } from "../fragments/types";
+import { Node } from "../layout/ir";
 
 export function switchOn(
     expr: string,
     ...branches: Branch[]
-): Document {
+): Node {
 
 }
