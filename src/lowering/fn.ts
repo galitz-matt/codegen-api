c391083353420be13d34a5fc2ff224b3a3eab71e
galitz-matt <galitz.matthew@gmail.com>
2026-02-21 15:00:54 -0500

    refactor: return node

--- src/lowering/fn.ts
+++ src/lowering/fn.ts
@@ -1,15 +1,12 @@
-import { Document, Node } from "../layout/ir";
+import { Node } from "../layout/ir";
 import { FnSignature } from "../fragments/types";
 import { block } from "../layout/factories";
 
 export function fn(
     signature: FnSignature,
-    body: Document
-): Document {
-    return [ 
-        block(
-            signature,
-            body
-        )
-    ]
+    ...body: Node[]
+): Node {
+    return block(signature,
+        body
+    )
 }
