8b6d98239896557a57791d0af64af2e08ee4a135
galitz-matt <galitz.matthew@gmail.com>
2026-02-26 21:32:25 -0500

    feat: introduce braceBlock sugar

--- src/layout/factories.ts
+++ src/layout/factories.ts
@@ -1,14 +1,15 @@
 import { Line, Node, Block, Seq } from "./ir";
 
 export function block(
-    header: string,
-    ...body: Node[]
+    open: string,
+    body: Node[],
+    close: string
 ): Block {
     return {
         kind: "block",
-        open: header + " {",
+        open,
         body,
-        close: "}"
+        close,
     }
 }
 
@@ -19,9 +20,22 @@ export function line(text: string): Line {
     }
 }
 
-export function seq(...nodes: (Line | Block)[]): Seq {
+export function seq(...nodes: Node[]): Seq {
     return {
         kind: "seq",
         nodes
     }
 }
+
+//#region sugar
+export function braceBlock(
+    header: string,
+    ...body: Node[]
+): Block {
+    return block(
+        header + " {",
+        body,
+        "}"
+    )
+}
+//#endregion
