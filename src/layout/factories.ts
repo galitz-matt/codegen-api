2ba40ad3eba6ee7a12918734ddf0a59ca7e61b0b
galitz-matt <galitz.matthew@gmail.com>
2026-03-03 20:05:56 -0500

    feat: narrow seq arg types

--- src/layout/factories.ts
+++ src/layout/factories.ts
@@ -20,7 +20,7 @@ export function line(text: string): Line {
     }
 }
 
-export function seq(...nodes: Node[]): Seq {
+export function seq(...nodes: (Line | Block)[]): Seq {
     return {
         kind: "seq",
         nodes
