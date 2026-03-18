cbb9db7fcd1c21343316cf94470a01df80e7c158
galitz-matt <galitz.matthew@gmail.com>
2026-03-17 21:24:54 -0400

    fix: syntax errors

--- src/lowering/switchOn.ts
+++ src/lowering/switchOn.ts
@@ -6,10 +6,10 @@ export function switchOn(
     expr: string,
     ...branches: [...CaseBranch[], DefaultBranch] | CaseBranch[] | [DefaultBranch] | []
 ): Node {
-    return braceBlock(`switch ${expr})`,
+    return braceBlock(`switch (${expr})`,
         ...branches.map(b => {
             if (b.kind === "case") {
-                return braceBlock(`case: ${b.value}`, 
+                return braceBlock(`case ${b.value}:`, 
                     ...b.body);
             } else {
                 return braceBlock(`default:`, 
